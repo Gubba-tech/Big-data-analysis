df359161d92ac9d617f9c684e5a0e1d3400f2a4c
Gubba-tech <75552608+Gubba-tech@users.noreply.github.com>
2021-02-16 02:28:20 -0500

    Add files via upload

---
+++ project1_10.R
@@ -0,0 +1,25 @@
+#! /gpfs/software/R-3.6.0/lib64/R/bin/Rscript --vanilla
+
+my_data <- read.csv("/gpfs/home/weicdeng/project/project1_data(1).csv")
+fit1 <- lm(y~., data =my_data)
+res <- fit1$residuals
+intercept <- rep(0,5000)
+beta1 <- rep(0,5000)
+beta2 <- rep(0,5000)
+beta3 <- rep(0,5000)
+beta4 <- rep(0,5000)
+beta5 <- rep(0,5000)
+for(i in 1:5000){
+  data1 <- my_data
+  choice <- sample(res, size= 100000, replace = T)
+  data1$y <- data1$y+choice
+  fit_re <- lm(y~., data = data1)
+  intercept[i] <- fit_re$coefficients[1]
+  beta1[i] <- fit_re$coefficients[2]
+  beta2[i] <- fit_re$coefficients[3]
+  beta3[i] <- fit_re$coefficients[4]
+  beta4[i] <- fit_re$coefficients[5]
+  beta5[i] <- fit_re$coefficients[6]
+}
+table10 <- data.frame(intercept,beta1,beta2,beta3,beta4,beta5)
+write.table(table10, 'table10.csv')
---
+++ project1_5.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#SBATCH --output=table5.csv
+#SBATCH --ntasks-per-node=40
+#SBATCH --nodes=2
+#SBATCH --time=10:00
+#SBATCH -p short-40core
+
+module load R/3.6.0
+module load shared
+module load intel/compiler/64/2017/17.0.0
+module load intel/mkl/64/2017/0.098
+module load intel/mpi/64/2017/0.098
+/gpfs/home/weicdeng/project/hw1/project1_5.R
